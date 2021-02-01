bb379e0f5d502614804c7deec374a78976b0cd92
Alejandro Rodriguez <alejandro.rodriguez@patelhs.org>
2021-02-01 14:42:53 +0000

    Changed the boolean so alive would be true changed the int method so it subtracts current and creation years

--- Main.java
+++ Main.java
@@ -6,32 +6,36 @@ class Main {
   
   //calls the 3 methods
   System.out.println("What is your business called?");
-    String businessName = scan.nextLine();
+    String companyName = scan.nextLine();
+    System.out.println("What is the current year?");
+    int currentDate = scan.nextInt();
   System.out.println("When was your business created? (Year only)");
-    int creationDate = scan.nextInt();
+    int creationDate1 = scan.nextInt();
   System.out.println("Is your business still a business? (true/false)");
     boolean stillABusi = scan.nextBoolean();
 
   //Prints input from Scanner
-  System.out.println(businessName);
-  System.out.println(creationDate);
+  System.out.println(company(companyName));
+  System.out.println(creationDate(currentDate, creationDate1) + " is the difference in years between your business creation and the current year.");
   if(stillABusi == true){
-    System.out.println("Your company is still going strong!");a
+    System.out.println(stillAlive(stillABusi) + "." + " Your company is still around.");
   }else{
     System.out.println("Your company is out of business. Sorry");
   }
-  
-  }
+}
   //Method returns name of business 
-  static String businessName(String name){
+  static String company(String name){
+    name = "Congratulations on having this company which is named " + name + "!";
     return name;
   } 
   //Method returns date when business was created
-  static int creationDate(int date){
-    return date;
+  static int creationDate(int date, int busiDate){
+     int date1 = date - busiDate;
+    return date1;
   }
   //Method returns if the business is still a business
-  static boolean stillABusi(boolean alive){
+  static boolean stillAlive(boolean alive){
+    alive = true;
     return alive;
   }
 }
